af31502d15b751e7b3d2ab8b1f4384b93bcab6a7
ma-ryu <54539186+ma-ryu@users.noreply.github.com>
2023-03-14 17:53:33 +0900

    [fix] aliasの設定を分離

---
+++ .config/fish/conf.d/alias.fish
@@ -0,0 +1,6 @@
+alias bat='batcat'
+alias ip='ip -color=auto'
+alias ls='lsd'
+alias ll='ls -l'
+alias la='ls -a'
+alias lla='ls -la'
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,20 +1,33 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+set -gx BROWSER "/mnt/c/Program Files/Google/Chrome/Application/chrome.exe"
+set -gx PAGER "less"
+
 set -gx XDG_CONFIG_HOME "$HOME/.config"
 set -gx XDG_CACHE_HOME "$HOME/.cache"
 set -gx XDG_DATA_HOME "$HOME/.local/share"
 set -gx XDG_STATE_HOME "$HOME/.local/state"
+
+# volta
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
+
+# rust
+set -gx PATH "$HOME/.cargo/bin:$PATH"
+
+# go
 set -gx GOPATH $HOME/go
 set -gx PATH $PATH $GOPATH/bin
+
+# direnv
 set -gx DIRENV_LOG_FORMAT ""
+
+# openssl
 set KERL_CONFIGURE_OPTIONS "--with-ssl=/opt/openssl/1.1.1t"
 
 set fish_greeting
-
-alias bat='batcat'
 
 #peco
 function fish_user_key_bindings
